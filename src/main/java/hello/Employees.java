7ab2765e7559bec1465b3494975d45b286f6b990
Chinmaya S <chinmaya@gmail.com>
2020-08-14 17:47:01 -0700

    update

---
+++ src/main/java/hello/Employees.java
@@ -0,0 +1,41 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Employees {
+	
+	@Id
+	@GeneratedValue
+	private Integer emp_no;
+	
+	private String first_name;
+	
+	private String last_name;
+
+	public Integer getEmp_no() {
+		return emp_no;
+	}
+
+	public void setEmp_no(Integer emp_no) {
+		this.emp_no = emp_no;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+}
